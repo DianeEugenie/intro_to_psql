be12c8b3aaacb0a003ab4ce5631715d29eee8849
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-30 09:42:57 +0100

    Adds notes regarding MVP and Extensions

--- marvel.sql
+++ marvel.sql
@@ -58,30 +58,36 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 
 -- Oops! Someone spelled Krusty The Clown's name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
 -- UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+-- UPDATE people SET name = 'Krusty the Clown' WHERE id = 13; -- better to use ID as it's unique
 
 -- Return ONLY Homer Simpson's name from the 'people' table.
 -- SELECT name FROM people WHERE id = 1;
 
 -- The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 -- DELETE FROM movies WHERE title = 'Batman Begins';
+-- DELETE FROM movies WHERE id = 9;
 
 -- We forgot one of the main characters! Add Bart Simpson to the 'people' table
 -- INSERT INTO people (name) VALUES ('Bart Simpson');
 
 -- Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
 -- DELETE FROM people WHERE name = 'Eric Cartman';
+-- DELETE FROM people WHERE id = 11;
 
 -- The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
 -- INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2019, '24:00');
 
 -- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
 -- SELECT show_time FROM movies WHERE title = 'Iron Man 2'; --18:45
+-- SELECT show_time FROM movies WHERE id = 3; --18:45
 -- UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
+-- UPDATE movies SET show_time = '20:45' WHERE id = 7;
 
 
 -- Research how to delete multiple entries from your table in a single command.
 -- DELETE FROM movies WHERE year <> 2017; -- delete all but the movies made in 2017
--- DELETE FROM people WHERE id IN (1, 2, 3); -- delete the rows with the id 1, 2 and 3 from people
--- DELETE FROM people WHERE id >= 1 and id <= 3; --delete the rows with the id 1, 2 and 3 from people
+-- DELETE FROM people WHERE id IN (1, 2, 3); -- delete the rows with the id 1, 2 and 3 from people IN = INCLUDES
+-- DELETE FROM people WHERE id >= 1 AND id <= 3; --delete the rows with the id 1, 2 and 3 from people
 -- DELETE FROM movies WHERE year BETWEEN 2008 AND 2012; -- deletes all movies made between 2008 and 2012 including.
--- DELETE FROM movies WHERE show_time BETWEEN '13:00' AND '22:55'; deeletes all movies showing betwee 1 and 22:55
+-- DELETE FROM movies WHERE show_time BETWEEN '13:00' AND '22:55'; deletes all movies showing betwee 1 and 22:55
+-- DELETE FROM people WHERE name LIKE '%Simpson'; -- deletes all the people with lastname Simpson don't care about anything that comes before it % is a wildcard character.
